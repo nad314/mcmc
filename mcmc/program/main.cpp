fb416b6ff9462ba9ff61f5f011eaed4ab4dc4fa0
nad314 <borna.mihaljevic@gmail.com>
2016-12-11 01:23:29 +0100

    moving to ubuntu

--- mcmc/program/main.cpp
+++
@@ -1,5 +0,0 @@
-#include <main>
-
-int WINAPI WinMain(HINSTANCE hI, HINSTANCE hPI, LPSTR cmdLine, int nCmdShow) {
-	return core::Init()(new MCMC());
-}
--- mcmc/renderWindow/renderWindow.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-class RenderWindow : public core::Form {
-public:
-	void onOpening() override;
-	int onPaint(const core::eventInfo& e) override;
-};
